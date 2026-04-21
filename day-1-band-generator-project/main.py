60f99d751421a802adb64cb232cef8ca778137af
codetime2026 <timothymullins2018@gmail.com>
2026-04-21 16:47:35 -0400

    initial commit

---
+++ day-1-band-generator-project/main.py
@@ -0,0 +1,4 @@
+print("Hello this is a band name generator.")
+city = input("What is the name of the city you grew up in?\n")
+pet = input("What is the name of your pet?\n")
+print("This is your band name. " + city + " " + pet)
